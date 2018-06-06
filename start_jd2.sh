1c31bca7f1ec926ac4fc6960dc404adabd85933b
flo313 <31136223+flo313@users.noreply.github.com>
2018-06-06 19:42:02 +0200

    Update start_jd2.sh

--- start_jd2.sh
+++ start_jd2.sh
@@ -12,10 +12,22 @@ echo "$(ts) Create user ${USER_NAME} (id ${USER_ID}) if needed..."
 adduser -D -u ${USER_ID} -G ${USER_NAME} -s /bin/sh -h /jdownloader ${USER_NAME} 2>/dev/null
 
 # Set directory permissions.
-echo "$(ts) Set user ${USER_NAME} (id ${USER_ID}) owner of /jdownloader and /downloads..."
-chown -R ${USER_NAME}:${USER_NAME} /jdownloader /downloads
-echo "$(ts) Set permissions of /downloads to 755..."
-chmod -R u+rwx /downloads
+#echo "$(ts) Set user ${USER_NAME} (id ${USER_ID}) owner of /jdownloader and /downloads..."
+#chown -R ${USER_NAME}:${USER_NAME} /jdownloader /downloads
+#echo "$(ts) Set permissions of /downloads to 755..."
+#chmod -R u+rwx /downloads
+
+echo "$(ts) Check user write access on folders (user: ${USER_NAME} id ${USER_ID})"
+for dir in /downloads; do
+  echo "$(ts)    Check $dir..."
+  if sudo su - $USER_NAME -c "[ -w $dir ]" ; then 
+    echo "$(ts)    Write access to $dir -> OK"
+  else
+    echo "$(ts)    /!\ Write access to $dir /!\ -> KO"
+    echo "$(ts)    Exiting script..."
+    exit
+  fi
+done 
 
 # Finally, start JDownloader.
 echo "$(ts) Starting JDownloader..."
