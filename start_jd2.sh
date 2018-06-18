bec5e80e7075162662555099861a0d7bed77d2f0
flo313 <31136223+flo313@users.noreply.github.com>
2018-06-18 17:32:45 +0200

    Update start_jd2.sh

--- start_jd2.sh
+++ start_jd2.sh
@@ -2,7 +2,9 @@
 function ts {
   echo "[`date '+%Y-%m-%d %T'`] `basename "$(test -L "$0" && readlink "$0" || echo "$0")"`:"
 }
-
+echo "*****************************************************************************************************"
+echo "$(ts) Start Script"
+echo ""
 # mkdir /jdownloader
 echo "$(ts) Create group ${USER_NAME} (id ${USER_ID}) if needed..."
 cat /etc/group | grep ${USER_NAME} >/dev/null 2>&1
@@ -14,14 +16,6 @@ cat /etc/passwd | grep ${USER_NAME} >/dev/null 2>&1
 if [ $? = 1 ] ; then
     adduser -D -u ${USER_ID} -G ${USER_NAME} -s /bin/sh -h /jdownloader ${USER_NAME} 2>/dev/null
 fi
-
-# Set directory permissions.
-#echo "$(ts) Set user ${USER_NAME} (id ${USER_ID}) owner of /jdownloader and /downloads..."
-#chown -R ${USER_NAME}:${USER_NAME} /jdownloader /downloads
-#echo "$(ts) Set user ${USER_NAME} (id ${USER_ID}) owner of /jdownloader..."
-#chown -R ${USER_NAME}:${USER_NAME} /jdownloader
-#echo "$(ts) Set permissions of /downloads to 755..."
-#chmod -R u+rwx /downloads
 
 echo "$(ts) Check user write access on folders (user: ${USER_NAME} id ${USER_ID})"
 for dir in /downloads /jdownloader ; do
@@ -37,6 +31,7 @@ done
 
 # Finally, start JDownloader.
 echo "$(ts) Starting JDownloader..."
-exec su -pc "exec java -Djava.awt.headless=true -jar /jdownloader/JDownloader.jar 2>&1 >/dev/null" $USER_NAME
+#exec su -pc "exec java -Djava.awt.headless=true -jar /jdownloader/JDownloader.jar 2>&1 >/dev/null" $USER_NAME
+exec su -pc "exec java -Djava.awt.headless=true -jar /jdownloader/JDownloader.jar" $USER_NAME
 
 #while sleep 3600; do :; done
